43c50f2e07b5c7b80718e3fd3b52eace888fdded
Oleg Sobolev <sobolev_ov@mail.ru>
2024-06-19 15:17:57 +0200

    Dependencies update

--- testing/test/build.gradle.kts
+++ testing/test/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-    manualImplementation("org.bouncycastle:bcmail-jdk18on:1.77")
+    manualImplementation("org.bouncycastle:bcmail-jdk18on:1.78.1")
     manualImplementation(project(":smime-simple"))
     manualImplementation(project(":smime-testing-bc"))
     manualImplementation(project(":smime-testing-rand"))
