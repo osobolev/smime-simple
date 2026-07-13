06c9815fb5407f88e91b9ed7cf267d737ebec3f3
Oleg Sobolev <sobolev_ov@mail.ru>
2026-07-13 02:20:39 +0200

    Dependency updates

--- testing/bc/build.gradle.kts
+++ testing/bc/build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
 
 dependencies {
     api(project(":smime-simple"))
-    api("org.bouncycastle:bcprov-jdk18on:1.84")
-    api("org.bouncycastle:bcpkix-jdk18on:1.84")
+    api("org.bouncycastle:bcprov-jdk18on:1.85")
+    api("org.bouncycastle:bcpkix-jdk18on:1.85")
 }
