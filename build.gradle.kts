005951c58cb51eaffb6bda0e7e46293090398812
Oleg Sobolev <sobolev_ov@mail.ru>
2026-07-29 11:46:18 +0200

    Dependency updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,3 @@
-plugins {
-    id("com.github.ben-manes.versions") version "0.54.0"
-}
-
 fun requiredMajor(mod: ModuleComponentIdentifier): String {
     if (mod.group == "com.sun.mail") return "1." // Version >= 2 uses jakarta namespace
     return ""
